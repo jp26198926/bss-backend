d1e75669a71e753ac38c5571f114048c9f3681bc
Jaypee Hindang <eujay.29@gmail.com>
2021-12-06 16:19:14 +1000

    Fix: change default status value of ticket and user

--- models/UserModel.js
+++ models/UserModel.js
@@ -13,7 +13,7 @@ const Schema = mongoose.Schema({
     },
     password: String,
     role: { type: mongoose.SchemaTypes.ObjectId, ref: 'Role' },
-    status: { type: mongoose.SchemaTypes.ObjectId, ref: 'Status'},
+    status: { type: mongoose.SchemaTypes.ObjectId, ref: 'Status', default: "61ad17cecc17fc1781ad99dc"},
 });
 
 const Model = mongoose.model('User', Schema);
