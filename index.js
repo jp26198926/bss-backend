5c50be8a790bb766ca61453d3fe6e2da0d669652
Jaypee Hindang <eujay.29@gmail.com>
2021-12-27 20:06:23 +1000

    Fix: cors

--- index.js
+++ index.js
@@ -15,24 +15,25 @@ const SettingRoutes = require("./routes/Setting");
 const KnowledgebaseRoutes = require("./routes/Knowledgebase");
 
 const app = express();
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+
 const PORT = process.env.PORT || 8283;
 dotenv.config();
-app.use(cors());
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested, Content-Type, Accept Authorization"
-//     )
-//     if (req.method === "OPTIONS") {
-//       res.header(
-//         "Access-Control-Allow-Methods",
-//         "POST, PUT, PATCH, GET, DELETE"
-//       )
-//       return res.status(200).json({})
-//     }
-//     next()
-//   })
 
 mongoose
   .connect(process.env.MONGO_URI, {
