08336d63c03e362c3a1163b7af29f80160ba2b16
Jaypee Hindang <eujay.29@gmail.com>
2021-12-27 18:52:20 +1000

    Fix: change default status value in ticketmodel.js

--- models/TicketModel.js
+++ models/TicketModel.js
@@ -1,29 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema({
-    ticketNo: {
-        type: String,
-        required: true,
-        trim: true,
-        default: () => { return Date.now().toString() },
-        index: {
-            unique: true,
-            collation: { locale: 'en', strength: 2 }
-        }
+  ticketNo: {
+    type: String,
+    required: true,
+    trim: true,
+    default: () => {
+      return Date.now().toString();
     },
-    createdAt: {
-        type: Date,
-        default: () => { return Date.now() }
+    index: {
+      unique: true,
+      collation: { locale: "en", strength: 2 },
     },
-    name: { type: String, required: true, trim: true },
-    email: {type: String,required: true, trim: true },
-    category: { type: mongoose.SchemaTypes.ObjectId, ref: 'TicketCategory' },
-    subject: { type: String, required: true, trim: true },
-    message: { type: String, required: true, trim: true },
-    assignedTech: { type: mongoose.SchemaTypes.ObjectId, ref: 'User', default: null},
-    status: { type: mongoose.SchemaTypes.ObjectId, ref: 'TicketStatus', default: "6194d2b3ce2dfc71dd58d3e1"}
+  },
+  createdAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
+    },
+  },
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true },
+  category: { type: mongoose.SchemaTypes.ObjectId, ref: "TicketCategory" },
+  subject: { type: String, required: true, trim: true },
+  message: { type: String, required: true, trim: true },
+  assignedTech: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  status: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "TicketStatus",
+    default: "61ada32dd43e6ce8fbf08b28",
+  },
 });
 
-const Model = mongoose.model('Ticket', Schema);
+const Model = mongoose.model("Ticket", Schema);
 
 module.exports = Model;
